6a7b24abc2a2b45ef2079302910e3c8f9d034826
Devem <Devem@126.com>
2023-03-27 23:39:49 +0800

    added refresh function to RecommendationScreen

--- app/src/main/java/com/example/campus_bbs/ui/RecommendationScreen.kt
+++ app/src/main/java/com/example/campus_bbs/ui/RecommendationScreen.kt
@@ -2,14 +2,19 @@ package com.example.campus_bbs.ui
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.pullrefresh.*
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -43,18 +48,39 @@ fun BlogsCard(
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterialApi::class)
 @Composable
 fun RecommendationScreen(
     mainAppNavController: NavHostController,
     modifier: Modifier = Modifier
 ) {
-    LazyColumn(
-        modifier = modifier,
-        verticalArrangement = Arrangement.spacedBy(5.dp)
-    ) {
-        items(6) {
-            BlogsCard({mainAppNavController.navigate("BlogScreen")})
-        }
+
+    var refreshing by remember { mutableStateOf(false) }
+    var itemCount by remember { mutableStateOf(1) }
+    val refreshScope = rememberCoroutineScope()
+
+    fun refresh () = refreshScope.launch {
+        refreshing = true
+        delay(1500)
+        itemCount += 1
+        refreshing = false
     }
+    val state = rememberPullRefreshState(refreshing, ::refresh)
+
+    Box(modifier = modifier.pullRefresh(state)) {
+        LazyColumn(
+            modifier = modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(5.dp)
+        ) {
+            items(itemCount) {
+                BlogsCard({mainAppNavController.navigate("BlogScreen")})
+            }
+        }
+        PullRefreshIndicator(refreshing = refreshing, state = state, modifier = Modifier.align(Alignment.TopCenter))
+    }
+
+
+
+
 }
