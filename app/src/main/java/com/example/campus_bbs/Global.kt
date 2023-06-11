b97fbba9a47d1d321905f5d7060c7f46b96d5ead
adfwer233 <ba123698745@126.com>
2023-06-11 14:14:52 +0800

    chore: ip in global

--- app/src/main/java/com/example/campus_bbs/Global.kt
+++ app/src/main/java/com/example/campus_bbs/Global.kt
@@ -1,6 +1,6 @@
 package com.example.campus_bbs
 
 object Global {
-    const val BASE_HTTP_URL = "http://183.172.129.177:8080"
-    const val BASE_WS_URL = "183.172.129.177"
+    const val BASE_HTTP_URL = "http://43.138.60.13:8080"
+    const val BASE_WS_URL = "43.138.60.13"
 }
